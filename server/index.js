7ebb6486fb01736af0c97d6378bb1b0859c2c284
Dodo <dmukunzi0@gmail.com>
2019-10-21 19:11:56 +0200

    ft(endpoints): add /GET all and /GET one endpoints [ Finishes 1234567 ]

--- server/index.js
+++ server/index.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import todos from './models/Todo.js';
 
 const app = express();
 
 app.get('/status', (req, res) => {
   res.send({ status: 'Active' });
+});
+
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    status: 'Success',
+    data: todos,
+  });
+});
+
+app.get('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find(found => found.id === id);
+  if (todo) {
+    res.status(200).send({
+      status: 'Success',
+      data: todo,
+    });
+  }
 });
 
 app.listen(3000, () => {
